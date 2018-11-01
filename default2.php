3278d888ac89ce8a21b77fe81b86da60457d238b
Jan van der Heide <jan@basecone.nl>
2018-11-01 20:37:46 +0100

    First try

---
+++ default2.php
@@ -0,0 +1,70 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<title>Natuurlijk Sophie</title>
+
+    <style type="text/css">
+        .column-left {
+            float: left;
+            width: 33%;
+        }
+
+        .column-right {
+            float: right;
+            width: 33%;
+        }
+
+        .column-center {
+            display: inline-block;
+            width: 33%;
+        }
+
+        #main {
+            border-color: green;
+            border-width: 3px;
+            border-style: solid;
+            text-align: center;
+            height: 400px;
+            font-size: 25px;
+        }
+        #spacer {
+            height: 100px;
+        }
+        .green {
+            color: green;
+        }
+    </style>  
+</head>
+
+ 
+<body>
+
+<div class="container">
+   <div class="column-center">
+       <div id="spacer">
+           &nbsp;
+       </div>
+		<div id="main">
+            <br />
+            <br />
+            <br />
+            <br />
+		    Hier komt de website van <span class="green">Natuurlijk Sophie.</span><br/>
+            <br />
+		    Contact: natuurlijksophie@gmail.com
+		    <br />
+            <br />
+		    <img src="images/blaadje.jpg" />
+		</div>
+       <p>
+           Natuurlijk Sophie &copy; 2015  | All Rights Reserved
+       </p>
+       
+   </div>
+   <div class="column-left"> &nbsp;</div>
+   <div class="column-right"> &nbsp;</div>
+</div>
+</body>
+</html>
